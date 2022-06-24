66687f4481558d4a2e3bc0afe1bed06558e1b48b
Jie Xu <xujie6427@gmail.com>
2022-06-25 00:54:49 +0100

    Update download_collection.sh

--- src/main/python/download_collection.sh
+++ src/main/python/download_collection.sh
@@ -1,15 +1,15 @@
 echo "Creating collection and duplicates directory..."
-mkdir -p files/raw_collection
-mkdir -p files/duplicates_file
+mkdir -p /exports/eddie/scratch/s1717425/files/raw_collection
+mkdir -p /exports/eddie/scratch/s1717425/files/duplicates_file
 
 echo "Begin downloading raw collections.."
 
-echo "Downloading WaPo...," 
-echo "Enter the password (if you don't have it, please visit https://trec.nist.gov/data/wapost/)"
-wget -c --user TRECWaPoSt --ask-password https://ir.nist.gov/wapo/WashingtonPost.v4.tar.gz -P files/raw_collection
+#echo "Downloading WaPo...," 
+#echo "Enter the password (if you don't have it, please visit https://trec.nist.gov/data/wapost/)"
+#wget -c --user TRECWaPoSt --ask-password https://ir.nist.gov/wapo/WashingtonPost.v4.tar.gz -P files/raw_collection
 
-echo "Downloading KILT..."
-wget -c http://dl.fbaipublicfiles.com/KILT/kilt_knowledgesource.json -P files/raw_collection
+#echo "Downloading KILT..."
+#wget -c http://dl.fbaipublicfiles.com/KILT/kilt_knowledgesource.json -P files/raw_collection
 
 echo "Downloading MARCO V2..."
 wget -c https://msmarco.blob.core.windows.net/msmarcoranking/msmarco_v2_doc.tar -P files/raw_collection
